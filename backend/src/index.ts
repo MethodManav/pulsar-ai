127ce23e70eac7041abe62c93dfcc6b5e5cf0c60
Behera Manav <manavbehera123@gmail.com>
2025-08-15 09:48:58 +0530

    fix(build):add types as dev-dependency

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,8 +14,8 @@ class CreateServer {
     this.app.use(cors());
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
-    this.app.listen(3000, () => {
-      console.log("Server is running on port 3000");
+    this.app.listen(5000, () => {
+      console.log("Server is running on port 5000");
     });
   }
 
